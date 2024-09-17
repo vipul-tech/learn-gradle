daa08b58a2715cbaabfe425077a3159a4575a387
kumavip <kumarvipul1513@gmail.com>
2024-09-17 13:13:36 +0530

    initial commit

---
+++ src/test/java/com/vipul/learn_gradle/LearnGradleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.vipul.learn_gradle;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LearnGradleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
